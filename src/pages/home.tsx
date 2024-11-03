60c937aca71063c41648a1fcfff85c1e337cb0d6
Katumbela <ja3328173@gmail.com>
2024-11-04 00:49:37 +0100

    fix: src/pages/home.tsx. Criação da função Home em src/pages/home.tsx; Adição de log no console em src/pages/home.tsx; Modificação de código em src/pages/home.tsx; Criação de uma constante em src/pages/home.tsx; Alteração em componente React em src/pages/home.tsx; Mudança de texto em um elemento HTML em src/pages/home.tsx; Adição de um novo estilo inline em src/pages/home.tsx; Alteração de conteúdo em array ou objeto em src/pages/home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
-// pages/index.js
+"use client"
+
+
 import Head from "next/head";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useSwipeable } from "react-swipeable";
 import styles2 from "@/styles/inicio.module.css";
 import styles from "@/styles/cards.module.css";
@@ -9,17 +11,53 @@ import Header from "@/components/head";
 import Menu from "@/components/menu";
 import CartaoMultischool, { CartaoEstudante } from "@/components/cards";
 import InstituicaoResume, { MultischoolResume } from "@/components/resumeHome";
+import { useAppSelector } from "@/hooks";
+import { selectUser } from "@/store";
+import { StringUtils } from "@/utils";
+import usePeriodicStudentUpdate from "@/hooks/usePeriodicStudentUpdate";
+import { TransactionService } from "@/services";
+import { Transaction } from "@/infra/interfacess";
+import { TransactionList } from "@/components/transactionTable/transactrionTable";
 
 
 
 export default function Home() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const student = useAppSelector(selectUser)
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
 
   const handlers = useSwipeable({
     onSwipedLeft: () => setCurrentIndex((prevIndex) => (prevIndex + 1) % 2),
     onSwipedRight: () =>
       setCurrentIndex((prevIndex) => (prevIndex - 1 + 2) % 2),
   });
+
+
+
+  const adhesionNumber = student?.adhesionNumber ? String(student.adhesionNumber) : "";
+  usePeriodicStudentUpdate({ studentAdhesionNumber: adhesionNumber });
+
+
+  const TransactsService = new TransactionService()
+
+  async function getTransacts() {
+    if (student?.account?._id) {
+      const datas = await TransactsService.getTransactionsByAccount(student.account._id);
+      return datas;
+    }
+    return [];
+  }
+
+  useEffect(() => {
+    async function fetchTransactions() {
+      const transacts = await getTransacts();
+      console.log(transacts);
+      setTransactions(transacts); // Atualizando o estado com as transações
+    }
+
+    fetchTransactions();
+  }, []);
 
   return (
     <div className={`${styles.container} ${styles2.container}`}>
@@ -32,29 +70,27 @@ export default function Home() {
         />
       </Head>
       <Header title="Início" />
-      <Top2 information="Olá, Ana Diogo" />
+      <Top2 information={`Olá, ${StringUtils.getFirstAndLastWord(student?.studentName ? student?.studentName : '')}`} />
+
       {/* Carousel container */}
       <div className={styles2.carouselContainer} {...handlers}>
         <div
           className={`${styles2.carousel} ${styles.cards} ${styles2.cards}`}
           style={{
-            transform: `translateX(calc(-${currentIndex * 100}% - ${
-              currentIndex * 20
-            }px))`,
+            transform: `translateX(calc(-${currentIndex * 100}% - ${currentIndex * 20
+              }px))`,
           }}
         >
           <div
-            className={`${styles2.cardWrapper} ${
-              currentIndex === 0 ? styles2.activeCard : ""
-            }`}
+            className={`${styles2.cardWrapper} ${currentIndex === 0 ? styles2.activeCard : ""
+              }`}
           >
             <CartaoMultischool />
           </div>
 
           <div
-            className={`${styles2.cardWrapper} ${
-              currentIndex === 1 ? styles2.activeCard : ""
-            }`}
+            className={`${styles2.cardWrapper} ${currentIndex === 1 ? styles2.activeCard : ""
+              }`}
           >
             <CartaoEstudante />
           </div>
@@ -65,13 +101,13 @@ export default function Home() {
         <ul>
           {currentIndex === 0 && (
             <>
-              <MultischoolResume/>
+              <TransactionList transactions={transactions} />
             </>
           )}
 
           {currentIndex === 1 && (
             <>
-              <InstituicaoResume></InstituicaoResume>
+              <InstituicaoResume />
             </>
           )}
         </ul>
