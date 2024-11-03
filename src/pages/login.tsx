c72ecd67f382b31171613cb284f2e43140d79135
Katumbela <ja3328173@gmail.com>
2024-11-04 00:49:38 +0100

    fix: src/pages/login.tsx. Criação da função Login em src/pages/login.tsx; Adição de log no console em src/pages/login.tsx; Modificação de código em src/pages/login.tsx; Alteração de texto em src/pages/login.tsx; Criação de uma constante em src/pages/login.tsx; Alteração em componente React em src/pages/login.tsx; Mudança de texto em um elemento HTML em src/pages/login.tsx; Alteração em funções de manipulação de eventos em src/pages/login.tsx; Alteração de conteúdo em array ou objeto em src/pages/login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,8 +2,65 @@ import login from "../styles/login.module.css";
 import Image from "next/image";
 import PurpleButton from "../components/buttons";
 import Link from "next/link";
+import { useAppDispatch } from "@/hooks";
+import { routes } from "@/infra";
+import { loginSuccess } from "@/store";
+import { showSuccessToast, showErrorToast, AlertUtils } from "@/utils";
+import { setLocalStorage } from "@/utils/local-storage";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { loginService } from "@/services";
+import Button from "../components/buttons";
 
 export default function Login() {
+
+  const [adhesionNumber, setAdhesionNumber] = useState('')
+  const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
+  const dispatch = useAppDispatch()
+
+  const handleSubmit = async () => {
+
+    if (!adhesionNumber || !password) {
+      AlertUtils.error("Preencha suas credenciais")
+      return
+    }
+
+    setLoading(true)
+    try {
+      const { access_token, student } = await loginService(adhesionNumber, password)
+      const studentDoc = student
+      console.log(studentDoc)
+      if (access_token && student) {
+        dispatch(loginSuccess({ access_token, student }))
+        //console.log(access_token)
+        setLocalStorage('token', access_token)
+        showSuccessToast('login efectuado com sucesso')
+        setLoading(false)
+        window.location.href = routes.HOME_ROUTE
+        return
+      }
+      else {
+        setLoading(false)
+
+        toast.error("Credenciais inválidas, tente novamente")
+
+      }
+
+
+
+      // Redirecionar aqui ou fazer algo após o login bem-sucedido
+
+    } catch (error) {
+
+      setLoading(false)
+      showErrorToast('Erro ao efetuar login')
+      console.error('Erro no login:', error)
+    }
+  }
+
+
+
   return (
     <>
       <div className={login.container}>
@@ -47,6 +104,8 @@ export default function Login() {
               />
             </div>
             <input
+              value={adhesionNumber}
+              onChange={(e) => setAdhesionNumber(e.target.value)}
               type="text"
               placeholder="Número de estudante"
               className={login.input}
@@ -65,6 +124,8 @@ export default function Login() {
             </div>
             <input
               type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               placeholder="PIN de acesso"
               className={login.input}
             />
@@ -82,10 +143,9 @@ export default function Login() {
         </div>
 
         <div className={login.cta}>
-          <PurpleButton
-            description="Iniciar sessão"
-            redirect="/home"
-          ></PurpleButton>
+
+          <Button loading={loading} onClick={handleSubmit} variant="purple" description="Iniciar Sessão" />
+
           <div className={login.links}>
             <Link href="/reset" className={login.purple}>
               Esqueci-me do PIN de acesso
