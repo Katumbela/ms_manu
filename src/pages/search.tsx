4fd52d019bf4b8e2de2ff7d37096a61e1bd3e5ca
Katumbela <ja3328173@gmail.com>
2024-11-04 00:49:41 +0100

    fix: src/pages/search.tsx. Criação da função SearchI em src/pages/search.tsx; Modificação de código em src/pages/search.tsx; Alteração de texto em src/pages/search.tsx; Criação de uma constante em src/pages/search.tsx; Alteração em componente React em src/pages/search.tsx; Mudança de texto em um elemento HTML em src/pages/search.tsx; Alteração em funções de manipulação de eventos em src/pages/search.tsx; Alteração de conteúdo em array ou objeto em src/pages/search.tsx

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,8 +1,53 @@
+
+
+import { School } from "@/infra/interfacess";
+import { SchoolService } from "@/services";
 import search from "../styles/search.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { type } from "os";
+import { useState, useEffect } from "react";
+import SchoolComponentSkeleton from "@/components/skeletons/school-skeleton";
+import SchoolComponent from "@/components/school_components/school-component";
 
 export default function SearchI() {
+
+  const searchParams = useSearchParams();
+  const query = searchParams.get('type');
+
+  const [schools, setSchools] = useState<School[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredSchools, setFilteredSchools] = useState<School[]>([]);
+  const [loadSchools, setLoadSchools] = useState(true)
+
+  useEffect(() => {
+    const fetchSchools = async () => {
+
+      const service = new SchoolService();
+      const data = await service.getSchoolsByType(query ? query : "iem");
+      setSchools(data);
+      setFilteredSchools(data);
+      setLoadSchools(false)
+    };
+
+    fetchSchools();
+  }, []);
+
+
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const term = e.target.value.toLowerCase();
+    setSearchTerm(term);
+    const filtered = schools.filter(school =>
+      school.schoolName.toLowerCase().includes(term)
+    );
+    setFilteredSchools(filtered);
+  };
+
+
+
+
   return (
     <>
       <div className={search.container}>
@@ -16,7 +61,7 @@ export default function SearchI() {
             priority
           />
           <div className={search.title}>
-            <p>Instituições de Ensino Superior</p>
+            <p>Instituições de Ensino Superior </p>
           </div>
         </Link>
 
@@ -33,7 +78,10 @@ export default function SearchI() {
               />
             </div>
             <input
-              type="text"
+
+              value={searchTerm}
+              onChange={handleSearch}
+              type="search"
               placeholder="Pesquisar por curso"
               className={search.input3}
             />
@@ -42,121 +90,32 @@ export default function SearchI() {
 
         <div className={search.filter}>
           <p className={search.purple}>Ordenar por:</p>
-          <p>290 resultados</p>
+          <p>{schools.length} resultado{schools.length <= 1 ? '' : 's'}</p>
         </div>
 
-        <div className={search.results}>
-          <div className={search.sres}>
-            <Link href="/detailsI" className={search.inner}>
-              <Image
-                className={search.logo}
-                src={"/img/isptec.svg"}
-                alt="ba2ck"
-                width={85}
-                height={85}
-                priority
-              />
-              <div className={search.infoI}>
-                <p className={search.name}>
-                  ISPTEC - Instituto Superior Politécnico de Tecnologias e
-                  Ciências
-                </p>
-                <p className={`${search.info2} ${search.purple}`}>
-                  <span className={search.nCursos}>8 cursos</span> -{" "}
-                  <span className={search.country}>Luanda</span>
-                </p>
-              </div>
-            </Link>
-          </div>
-          <div className={search.sres}>
-            <Link href="/detailsI" className={search.inner}>
-              <Image
-                className={search.logo}
-                src={"/img/isptec.svg"}
-                alt="ba2ck"
-                width={85}
-                height={85}
-                priority
-              />
-              <div className={search.infoI}>
-                <p className={search.name}>
-                  ISPTEC - Instituto Superior Politécnico de Tecnologias e
-                  Ciências
-                </p>
-                <p className={`${search.info2} ${search.purple}`}>
-                  <span className={search.nCursos}>8 cursos</span> -{" "}
-                  <span className={search.country}>Luanda</span>
-                </p>
-              </div>
-            </Link>
-          </div>
-          <div className={search.sres}>
-            <Link href="/detailsI" className={search.inner}>
-              <Image
-                className={search.logo}
-                src={"/img/isptec.svg"}
-                alt="ba2ck"
-                width={85}
-                height={85}
-                priority
-              />
-              <div className={search.infoI}>
-                <p className={search.name}>
-                  ISPTEC - Instituto Superior Politécnico de Tecnologias e
-                  Ciências
-                </p>
-                <p className={`${search.info2} ${search.purple}`}>
-                  <span className={search.nCursos}>8 cursos</span> -{" "}
-                  <span className={search.country}>Luanda</span>
-                </p>
-              </div>
-            </Link>
-          </div>
-          <div className={search.sres}>
-            <Link href="/detailsI" className={search.inner}>
-              <Image
-                className={search.logo}
-                src={"/img/isptec.svg"}
-                alt="ba2ck"
-                width={85}
-                height={85}
-                priority
-              />
-              <div className={search.infoI}>
-                <p className={search.name}>
-                  ISPTEC - Instituto Superior Politécnico de Tecnologias e
-                  Ciências
-                </p>
-                <p className={`${search.info2} ${search.purple}`}>
-                  <span className={search.nCursos}>8 cursos</span> -{" "}
-                  <span className={search.country}>Luanda</span>
-                </p>
-              </div>
-            </Link>
-          </div>
-          <div className={search.sres}>
-            <Link href="/detailsI" className={search.inner}>
-              <Image
-                className={search.logo}
-                src={"/img/isptec.svg"}
-                alt="ba2ck"
-                width={85}
-                height={85}
-                priority
-              />
-              <div className={search.infoI}>
-                <p className={search.name}>
-                  ISPTEC - Instituto Superior Politécnico de Tecnologias e
-                  Ciências
-                </p>
-                <p className={`${search.info2} ${search.purple}`}>
-                  <span className={search.nCursos}>8 cursos</span> -{" "}
-                  <span className={search.country}>Luanda</span>
-                </p>
-              </div>
-            </Link>
-          </div>
+
+
+        <div className={'block w-full container'}>
+
+          {
+            loadSchools ?
+              <>
+                <SchoolComponentSkeleton />
+              </>
+              :
+              <>
+                {
+                  filteredSchools.map((course, i) => (
+                    <SchoolComponent data={course} index={i} key={i} />
+                  ))
+                }
+              </>
+          }
         </div>
+
+
+        <br />
+        <br />
       </div>
     </>
   );
