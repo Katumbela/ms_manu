cfa858e0de9a49b562e5eda94d2cf615aedfdff5
Katumbela <ja3328173@gmail.com>
2024-11-03 10:36:34 +0100

    feat: src/components/bg_explore.tsx. Criação de novo arquivo

---
+++ src/components/bg_explore.tsx
@@ -0,0 +1,9 @@
+import shapes from "@/styles/shapes.module.css";
+
+const BackgroundShapes = () => {
+  return (
+    <div className={shapes.bg}>
+      <div className={shapes.first}></div>
+    </div>
+  );
+};
