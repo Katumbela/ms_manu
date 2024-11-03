79c5bb86530a8ab92e1d83f0090e0276c3475e96
Katumbela <ja3328173@gmail.com>
2024-11-03 12:15:04 +0100

    fix: src/pages/_document.tsx. Criação da função Document em src/pages/_document.tsx; Modificação de código em src/pages/_document.tsx; Alteração em componente React em src/pages/_document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,11 +2,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt">
       <Head />
       <body>
-        <Main />
-        <NextScript />
+      
+              <Main />
+              <NextScript /> 
+        
       </body>
     </Html>
   );
