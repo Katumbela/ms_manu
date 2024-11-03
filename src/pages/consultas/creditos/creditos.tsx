0f680d9b5be4016cb58e32e3a8df04f69ac8c708
Katumbela <ja3328173@gmail.com>
2024-11-04 00:49:30 +0100

    fix: src/pages/consultas/creditos/creditos.tsx. Criação da função Credito em src/pages/consultas/creditos/creditos.tsx; Adição de log no console em src/pages/consultas/creditos/creditos.tsx; Modificação de código em src/pages/consultas/creditos/creditos.tsx; Alteração de texto em src/pages/consultas/creditos/creditos.tsx; Criação de uma constante em src/pages/consultas/creditos/creditos.tsx; Alteração em componente React em src/pages/consultas/creditos/creditos.tsx; Mudança de texto em um elemento HTML em src/pages/consultas/creditos/creditos.tsx; Alteração de conteúdo em array ou objeto em src/pages/consultas/creditos/creditos.tsx

--- src/pages/consultas/creditos/creditos.tsx
+++ src/pages/consultas/creditos/creditos.tsx
@@ -6,8 +6,48 @@ import payC from "@/styles/payments/instituicao/confirmacao2.module.css";
 import Menu from "@/components/menu";
 import Image from "next/image";
 import Link from "next/link";
+import usePeriodicStudentUpdate from "@/hooks/usePeriodicStudentUpdate";
+import { CreditRequest, Transaction } from "@/infra/interfacess";
+import { CreditRequestService, TransactionService } from "@/services";
+import { useState, useEffect } from "react";
+import { selectUser } from "@/store";
+import { useAppSelector } from "@/hooks";
+import { DateUtils, NumberUtils, StringUtils } from "@/utils";
+import { getStatusColor } from "@/utils/getStatusColor.utils";
 
 export default function Credito() {
+
+  const student = useAppSelector(selectUser)
+  const account = student?.account
+
+  const [myCredits, setMyCredits] = useState<CreditRequest[]>([])
+
+  const adhesionNumber = student?.adhesionNumber ? String(student.adhesionNumber) : "";
+  usePeriodicStudentUpdate({ studentAdhesionNumber: adhesionNumber });
+
+
+  const creditsService = new CreditRequestService()
+
+  async function getCredits() {
+    if (account) {
+      const datas = await creditsService.getCreditRequestHistory(account.account_number);
+      return datas;
+    }
+    return [];
+  }
+
+  useEffect(() => {
+    async function fetchTransactions() {
+      const transacts = await getCredits();
+      // console.log(transacts);
+      setMyCredits(transacts);
+    }
+
+    fetchTransactions();
+  }, []);
+
+  const lastCredit = myCredits[myCredits.length - 1]
+
   return (
     <>
       <div className={cred.container}>
@@ -17,33 +57,34 @@ export default function Credito() {
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
           />
         </Head>
-        <Header title="Créditos Multischool"></Header>
+        <Header title="Créditos Multischool" />
         <Top information="Créditos" pagina="/consultsM"></Top>
 
         <div className={cred.first}>
           <div className={cred.inner1}>
             <Image src={"/avatars/ana.svg"} width={80} height={80} alt="" />
             <p>O seu crédito actual é:</p>
-            <h2>35.000,00 kz</h2>
+            <h2>{lastCredit ? NumberUtils.formatCurrency(lastCredit.amount) : "Nenhum crédito disponível"}</h2>
             <small>
               Último crédito feito em:{" "}
               <span className={cred.lastDate}>
-                22 de Agosto de 2024 às 10:44:50
+                {lastCredit ? DateUtils.getDateTimePt(lastCredit.createdAt) : "N/A"}
               </span>
             </small>
+
           </div>
           <div className={cred.inner2}>
             <div className={cred.in}>
               <p className={cred.in1}>Prazo</p>
-              <p className={cred.in2}>17 de Setembro de 2024</p>
+              <p className={cred.in2}>{lastCredit ? DateUtils.getDateTimePt(lastCredit.createdAt) : "N/A"}</p>
             </div>
             <div className={cred.in}>
-              <p className={cred.in1}>Valor</p>
-              <p className={cred.in2}>20.000,00 kz</p>
+              <p className={cred.in1}>Valor a pagar</p>
+              <p className={cred.in2}>{lastCredit ? NumberUtils.formatCurrency(lastCredit.amount + lastCredit.amount * .1) : "N/A"}</p>
             </div>
             <div className={cred.in}>
               <p className={cred.in1}>Total de créditos</p>
-              <p className={cred.in2}>3</p>
+              <p className={cred.in2}>{myCredits.length}</p>
             </div>
           </div>
         </div>
@@ -81,63 +122,37 @@ export default function Credito() {
             <p>Recentes</p>
             <p className={payC.subtitle}>Estado</p>
           </div>
-          <div className={payC.details}>
-            <div className={payC.icon}>
-              <Image
-                className={payC.avatar}
-                src={"/icons/cons/money.svg"}
-                alt="ba2ck"
-                width={30}
-                height={30}
-                priority
-              />
-            </div>
-            <div className={payC.desc}>
-              <p>Kwik Pag. Inst.</p>
-              <small>05.07.2024</small>
-            </div>
-            <div className={payC.amount}>
-              <p className={`${payC.price} ${payC.danger}`}>Rejeitado</p>
-            </div>
-          </div>
-          <div className={payC.details}>
-            <div className={payC.icon}>
-              <Image
-                className={payC.avatar}
-                src={"/icons/cons/money.svg"}
-                alt="ba2ck"
-                width={30}
-                height={30}
-                priority
-              />
-            </div>
-            <div className={payC.desc}>
-              <p>Kwik Pag. Inst.</p>
-              <small>05.07.2024</small>
-            </div>
-            <div className={payC.amount}>
-              <p className={`${payC.price} ${cred.success}`}>Confirmado</p>
-            </div>
-          </div>
-          <div className={payC.details}>
-            <div className={payC.icon}>
-              <Image
-                className={payC.avatar}
-                src={"/icons/cons/money.svg"}
-                alt="ba2ck"
-                width={30}
-                height={30}
-                priority
-              />
-            </div>
-            <div className={payC.desc}>
-              <p>Kwik Pag. Inst.</p>
-              <small>05.07.2024</small>
-            </div>
-            <div className={payC.amount}>
-              <p className={`${payC.price} ${payC.warning}`}>Pendente</p>
-            </div>
-          </div>
+
+          {
+            myCredits.map((credit, i) =>
+            (
+              <div key={i} className={payC.details}>
+                <div className={payC.icon}>
+                  <Image
+                    className={payC.avatar}
+                    src={"/icons/cons/money.svg"}
+                    alt="ba2ck"
+                    width={30}
+                    height={30}
+                    priority
+                  />
+                </div>
+                <div className={payC.desc}>
+                  <p>{credit.creditor ? credit.creditor : "Anonymous"}</p>
+                  <small className="text-[11px]">{DateUtils.getDateTimePt(credit.createdAt)}</small>
+                </div>
+                <div className={""}>
+                  <p className={``}>
+                    <span className={`text-xs font-bold tracking-wider ${getStatusColor(credit.status ? credit.status : "denied")}`}>
+                      {credit.status === 'approved' ? 'Aprovada' : cred.status === 'success' ? 'Sucesso' : credit.status === 'denied' ? 'Rejeitado' : 'Pendente'}
+                    </span>
+                  </p>
+                </div>
+              </div>
+            ))
+          }
+
+
         </div>
         <Menu></Menu>
       </div>
