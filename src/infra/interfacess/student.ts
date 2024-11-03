05bacdc7eb959e85f69e34c3754afa813e2a8c5a
Katumbela <ja3328173@gmail.com>
2024-11-03 19:25:33 +0100

    fix: src/infra/interfacess/student.ts. Modificação de código em src/infra/interfacess/student.ts

--- src/infra/interfacess/student.ts
+++ src/infra/interfacess/student.ts
@@ -6,15 +6,15 @@ import type { Grade } from "./grade";
 export interface Student {
   _id?: number;
   adhesionNumber?: number;
-  registrationNumber: string;
+  registrationNumber?: string;
   studentName: string;
-  class: string;
-  shift: string;
+  class?: string;
+  shift?: string;
   phoneNumber: string;
   course: string;
-  studentEmail: string
-  identificationNumber: string;
-  registrationYear: string;
+  studentEmail?: string
+  identificationNumber?: string;
+  registrationYear?: string;
   password: string;
   grades?: Grade[];
   account?: Account;
