23569fa626fb9692c5fa22f697b231025c2bbfbb
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-24 11:57:31 +0100

    feat: src/infra/interfacess/material.ts. Criação de novo arquivo

---
+++ src/infra/interfacess/material.ts
@@ -0,0 +1,10 @@
+import type { Discipline } from "./discipline";
+
+export interface Material {
+  id: string;;
+  label: string;
+  description: string;
+  link: string;
+  date: Date;
+  discipline: Discipline;
+}
