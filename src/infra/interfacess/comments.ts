b1f6b967a80e53f5cae68c465c90c97eb6b6783d
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-24 11:57:23 +0100

    feat: src/infra/interfacess/comments.ts. Criação de novo arquivo

---
+++ src/infra/interfacess/comments.ts
@@ -0,0 +1,9 @@
+import type { Lesson } from "./lesson";
+import type { Student } from "./student";
+
+export interface Comment {
+  id: string;;
+  content: string;
+  lesson: Lesson;
+  student: Student;
+}
