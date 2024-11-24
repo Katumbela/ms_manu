444b5a4ce797ef2152e906dbfdda7d3dd281d2dd
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-24 11:54:06 +0100

    feat: next.config.js. Criação de novo arquivo

---
+++ next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        // destination: 'https://multischooll-api-nest.vercel.app/:path*',
+        destination: 'http://localhost:3500/:path*',
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
