f62e3b892ca76ec79337e88a889146a1733cea38
Joao Afonso Katombela <ja3328173@gmail.com>
2024-11-24 11:57:23 +0100

    feat: src/infra/interfacess/company.ts. Criação de novo arquivo

---
+++ src/infra/interfacess/company.ts
@@ -0,0 +1,10 @@
+export interface Company {
+  id: string;;
+  companyName: string;
+  foundedYear: number;
+  category: string;
+  founderName: string;
+  biography: string;
+  contact: string;
+  email: string;
+}
